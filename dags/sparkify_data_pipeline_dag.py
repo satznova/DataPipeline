826352002645c5f3c383bd0ecd8a219ea00bd033
satznova <satznova@gmail.com>
2019-06-10 18:02:35 +0530

    logic for dag config file and modfied data quality op to handle list of tables

--- dags/sparkify_data_pipeline_dag.py
+++ dags/sparkify_data_pipeline_dag.py
@@ -1,77 +1,109 @@
 from datetime import datetime, timedelta
 import os
 from airflow import DAG
+from airflow.models import Variable
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                                LoadDimensionOperator, DataQualityOperator)
+from airflow.operators.udacity_plugin import (StageToRedshiftOperator, LoadFactOperator,
+                                                LoadDimensionOperator, DataQualityOperator)
+
 from helpers import SqlQueries
-
-#AWS_KEY = os.environ.get('AWS_KEY')
-#AWS_SECRET = os.environ.get('AWS_SECRET')
-
 
 # default arguments used when creating tasks
 default_args = {
     'owner': 'sathish',
     'depends_on_past': False,
-    'start_date': datetime(2019, 1, 1),
+    'start_date': datetime(2018, 11, 1),
+    'end_date': datetime(2018, 11, 30),
     'retries': 3,
     'retry_delay': timedelta(minutes=5),
-    'email_on_retry': False
+    'email_on_retry': False,
+    'catchup' : True
 }
 
-dag = DAG('udacity_dend_pipeline_dag',
+# defining dag object
+dag = DAG('sparkify_dwh_pipeline_dag',
 		  catchup=False,
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
+          schedule_interval='0 */1 * * *'
         )
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
+# staging from S3 to Redshift stage tables
+
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    dag=dag
+    dag=dag,
+    aws_credentials = 'aws_credentials',
+    redshift_conn_id = 'aws_redshift',
+    s3_bucket = Variable.get('s3_bucket'),
+    s3_key = Variable.get('s3_prefix_events'),
+    table_name = 'staging_events',
+    table_format = 'JSON'
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
+    aws_credentials = 'aws_credentials',
+    redshift_conn_id = 'aws_redshift',
+    s3_bucket = Variable.get('s3_bucket'),
+    s3_key = Variable.get('s3_prefix_events'),
+    table_name = 'staging_songs',
+    table_format = 'JSON'
 )
+
+# loading fact and dimension tables
 
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id = 'aws_redshift',
+    table_name = 'songplays',
+    sql_load_query = SqlQueries.songplay_table_insert
 )
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id = 'aws_redshift',
+    table_name = 'users',
+    sql_load_query = SqlQueries.user_table_insert
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id = 'aws_redshift',
+    table_name = 'songs',
+    sql_load_query = SqlQueries.song_table_insert
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id = 'aws_redshift',
+    table_name = 'artists',
+    sql_load_query = SqlQueries.artist_table_insert
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id = 'aws_redshift',
+    table_name = 'time',
+    sql_load_query = SqlQueries.time_table_insert
 )
 
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
-    dag=dag
+    dag=dag,
+    table_list = ['songplays', 'users', 'songs', 'artists', 'time'],
+    redshift_conn_id = 'aws_redshift'
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
-
-
 
 # configuring the Task Dependencies
 
--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -10,32 +10,35 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 table_name = "",
-                 conn_id = "",
+                 table_list = [],
+                 redshift_conn_id = "",
                  *args,
                  **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        self.table_name = table_name
-        self.redshift_conn_id = conn_id
+
+        self.table_list = table_list
+        self.redshift_conn_id = redshift_conn_id
 
     def execute(self, context):
         """Logic to check data quality - if records got loaded in the table"""
 
         try:
             logging.info('START: Data Quality Check - Started Execution')
-
             redshift_hook = PostgresHook(self.redshift_conn_id)
-            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {self.table_name}")
 
-            if(len(records) < 1 or len(records[0]) < 1):
-                raise ValueError(f"Data Quality Check Failed. {self.table_name} returned no results")
+            for table in self.table_list:
+                logging.info(f"Row number Check for Table: '{table}'")
+                records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
 
-            num_records = records[0][0]
-            if(num_records < 1):
-                raise ValueError(f"Data Quality Check Failed. {self.table_name} contained 0 rows")
+                if(len(records) < 1 or len(records[0]) < 1):
+                    raise ValueError(f"Data Quality Check Failed. '{table}' returned no results")
 
-            logging.info(f"SUCCESS: Data Quality Check PASSED with {num_records} records - Finished Execution")
+                num_records = records[0][0]
+                if(num_records < 1):
+                    raise ValueError(f"Data Quality Check Failed. '{table}' contained 0 rows")
+
+                logging.info(f"SUCCESS: Data Quality Check PASSED with {num_records} records - Finished Execution")
 
         except Exception as ex:
             logging.info(f"FAILED: Data Quality Check with error: {ex}")
